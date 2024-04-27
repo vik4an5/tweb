05062060a458ea5c52020a06a1fc932a75090dd8
vik4an5 <168275876+vik4an5@users.noreply.github.com>
2024-04-27 19:16:56 +0530

    script.js

---
+++ script.js
@@ -0,0 +1,396 @@
+var users = [
+   {
+      profilePic: "https://images.unsplash.com/photo-1628657487151-055f43d70667?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+
+      displayPic: "https://images.unsplash.com/photo-1574526769017-3287bfebe35f?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      pendingMessage: 64,
+      location: "Delhi, India",
+      name: "Harshita",
+      age: 23,
+      intersts: [{
+         icon: `<i class="ri-music-2-line"></i>`,
+         interest: "music"
+      }, {
+         icon: `<i class="ri-quill-pen-fill"></i>`,
+         interest: "painting"
+      }],
+      bio: "Katie is a seasoned marketing professional with over 10 years of experience in digital advertising strategies. She has helped numerous brands achieve their online marketing goals, leading to increased visibility and 3x revenue Yo ",
+      isFriends: null
+
+   },
+
+   {
+      profilePic: "https://images.unsplash.com/photo-1582758420652-97455135fb09?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjB8fHBvcnRhcml0fGVufDB8fDB8fHww",
+      displayPic: "https://images.unsplash.com/photo-1628657485502-38a19243392b?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      pendingMessage: 10,
+      location: "Kolkata, India",
+      name: "waiter",
+      age: 18,
+      intersts: [{
+         icon: `<i class="ri-music-2-line"></i>`,
+         interest: "music"
+      }, {
+         icon: `<i class="ri-quill-pen-fill"></i>`,
+         interest: "Dancing"
+      }],
+      bio: "!st year student at netaji subhash engineering college in information technology hobbies are dancing and singing ",
+      isFriends: null
+
+   },
+
+   {
+      profilePic: "https://images.unsplash.com/photo-1601049506376-8ca694a7e673?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDE2fHRvd0paRnNrcEdnfHxlbnwwfHx8fHw%3D",
+      displayPic: "https://images.unsplash.com/photo-1695392175592-2dcb9103f8ce?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mjd8fHBvcnRhcml0fGVufDB8fDB8fHww",
+      pendingMessage: 14,
+      location: "Kolkata, India",
+      name: "Abhay",
+      age: 18,
+      intersts: [{
+         icon: `<i class="ri-music-2-line"></i>`,
+         interest: "music"
+      }, {
+         icon: `<i class="ri-quill-pen-fill"></i>`,
+         interest: "painting"
+      }],
+      bio: "lorem24 grgdrgr ergedrgreg ergertgertg drgergertg ertgertgwerfg ergerg erger ge tger ge g ghr tgr ger 4gyrt ger th rgh ",
+      isFriends: null
+
+   },
+
+   {
+      profilePic: "https://images.unsplash.com/photo-1566228075280-70e167d08f78?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDE0fHRvd0paRnNrcEdnfHxlbnwwfHx8fHw%3D",
+      displayPic: "https://images.unsplash.com/photo-1610276198568-eb6d0ff53e48?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cG90cmFpdHxlbnwwfHwwfHx8MA%3D%3D",
+      pendingMessage: 61,
+      location: "Delhi, India",
+      name: "Vivek",
+      age: 24,
+      intersts: [{
+         icon: `<i class="ri-music-2-line"></i>`,
+         interest: "music"
+      }, {
+         icon: `<i class="ri-quill-pen-fill"></i>`,
+         interest: "painting"
+      }],
+      bio: "lorem24 grgdrgr ergedrgreg ergertgertg drgergertg ertgertgwerfg ergerg erger ge tger ge g ghr tgr ger 4gyrt ger th rgh ",
+      isFriends: null
+
+   },
+
+   {
+      profilePic: "https://unsplash.com/photos/woman-wearing-yellow-coat-I03ZQjTt4WI",
+      displayPic: "https://images.unsplash.com/photo-1552234994-66ba234fd567?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8cG90cmFpdHxlbnwwfHwwfHx8MA%3D%3D",
+      pendingMessage: 61,
+      location: "Delhi, India",
+      name: "Rakesh",
+      age: 24,
+      intersts: [{
+         icon: `<i class="ri-music-2-line"></i>`,
+         interest: "music"
+      }, {
+         icon: `<i class="ri-quill-pen-fill"></i>`,
+         interest: "painting"
+      }],
+      bio: "Katie is a seasoned marketing professional with over 10 years of experience in digital advertising strategies. She has helped numerous brands achieve their online marketing goals, leading to increased visibility and 3x revenue Yo ",
+      isFriends: null
+
+   },
+
+   {
+      profilePic: "https://images.unsplash.com/photo-1544463914-80667a08622d?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDExfHRvd0paRnNrcEdnfHxlbnwwfHx8fHw%3D",
+      displayPic: "https://images.unsplash.com/photo-1568602471122-7832951cc4c5?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8cG90cmFpdHxlbnwwfHwwfHx8MA%3D%3D",
+      pendingMessage: 61,
+      location: "Delhi, India",
+      name: "Amisha",
+      age: 24,
+      intersts: [{
+         icon: `<i class="ri-music-2-line"></i>`,
+         interest: "music"
+      }, {
+         icon: `<i class="ri-quill-pen-fill"></i>`,
+         interest: "painting"
+      }],
+      bio: "Katie is a seasoned marketing professional with over 10 years of experience in digital advertising strategies. She has helped numerous brands achieve their online marketing goals, leading to increased visibility and 3x revenue Yo ",
+      isFriends: null
+
+   },
+
+   {
+      profilePic: "https://images.unsplash.com/photo-1544408649-a2f9b76ca32d?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDEwfHRvd0paRnNrcEdnfHxlbnwwfHx8fHw%3D",
+      displayPic: "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8cG90cmFpdHxlbnwwfHwwfHx8MA%3D%3D",
+      pendingMessage: 61,
+      location: "Delhi, India",
+      name: "Tamanna",
+      age: 24,
+      intersts: [{
+         icon: `<i class="ri-music-2-line"></i>`,
+         interest: "music"
+      }, {
+         icon: `<i class="ri-quill-pen-fill"></i>`,
+         interest: "painting"
+      }],
+      bio: "Katie is a seasoned marketing professional with over 10 years of experience in digital advertising strategies. She has helped numerous brands achieve their online marketing goals, leading to increased visibility and 3x revenue Yo ",
+      isFriends: null
+
+   },
+
+   {
+      profilePic: "https://images.unsplash.com/photo-1540396776447-f2a5b9a020cf?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDd8dG93SlpGc2twR2d8fGVufDB8fHx8fA%3D%3D",
+      displayPic: "https://images.unsplash.com/photo-1531384441138-2736e62e0919?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fHBvdHJhaXR8ZW58MHx8MHx8fDA%3D",
+      pendingMessage: 61,
+      location: "Delhi, India",
+      name: "Prittyy",
+      age: 24,
+      intersts: [{
+         icon: `<i class="ri-music-2-line"></i>`,
+         interest: "music"
+      }, {
+         icon: `<i class="ri-quill-pen-fill"></i>`,
+         interest: "painting"
+      }],
+      bio: "Katie is a seasoned marketing professional with over 10 years of experience in digital advertising strategies. She has helped numerous brands achieve their online marketing goals, leading to increased visibility and 3x revenue Yo ",
+      isFriends: null
+
+   },
+
+   {
+      profilePic: "https://images.unsplash.com/photo-1659987606990-1bf4f47ed3e2?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDZ8dG93SlpGc2twR2d8fGVufDB8fHx8fA%3D%3D",
+      displayPic: "https://images.unsplash.com/photo-1492288991661-058aa541ff43?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fHBvdHJhaXR8ZW58MHx8MHx8fDA%3D",
+      pendingMessage: 61,
+      location: "Delhi, India",
+      name: "Kajal",
+      age: 24,
+      intersts: [{
+         icon: `<i class="ri-music-2-line"></i>`,
+         interest: "music"
+      }, {
+         icon: `<i class="ri-quill-pen-fill"></i>`,
+         interest: "painting"
+      }],
+      bio: "Katie is a seasoned marketing professional with over 10 years of experience in digital advertising strategies. She has helped numerous brands achieve their online marketing goals, leading to increased visibility and 3x revenue Yo ",
+      isFriends: null
+
+   },
+
+   {
+      profilePic: "https://images.unsplash.com/photo-1654850900859-be34bd2e217c?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjZ8fHBvcnRhcml0fGVufDB8fDB8fHww",
+      displayPic: "https://plus.unsplash.com/premium_photo-1674069719655-7289402d71e9?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTN8fHBvdHJhaXR8ZW58MHx8MHx8fDA%3D",
+      pendingMessage: 61,
+      location: "Delhi, India",
+      name: "Shivam",
+      age: 24,
+      intersts: [{
+         icon: `<i class="ri-music-2-line"></i>`,
+         interest: "music"
+      }, {
+         icon: `<i class="ri-quill-pen-fill"></i>`,
+         interest: "painting"
+      }],
+      bio: "Katie is a seasoned marketing professional with over 10 years of experience in digital advertising strategies. She has helped numerous brands achieve their online marketing goals, leading to increased visibility and 3x revenue Yo ",
+      isFriends: null
+
+   },
+
+   {
+      profilePic: "https://images.unsplash.com/photo-1713208177460-7b840b5b3119?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDR8dG93SlpGc2twR2d8fGVufDB8fHx8fA%3D%3D",
+      displayPic: "https://images.unsplash.com/photo-1566753323558-f4e0952af115?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTV8fHBvdHJhaXR8ZW58MHx8MHx8fDA%3D",
+      pendingMessage: 61,
+      location: "Delhi, India",
+      name: "Suraj",
+      age: 24,
+      intersts: [{
+         icon: `<i class="ri-music-2-line"></i>`,
+         interest: "music"
+      }, {
+         icon: `<i class="ri-quill-pen-fill"></i>`,
+         interest: "painting"
+      }],
+      bio: "Katie is a seasoned marketing professional with over 10 years of experience in digital advertising strategies. She has helped numerous brands achieve their online marketing goals, leading to increased visibility and 3x revenue Yo ",
+      isFriends: null
+
+   },
+
+   {
+      profilePic: "https://images.unsplash.com/photo-1701706291994-5d8967b45294?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDN8dG93SlpGc2twR2d8fGVufDB8fHx8fA%3D%3D",
+      displayPic: "https://plus.unsplash.com/premium_photo-1674639437824-771a65f1738b?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8cG90cmFpdHxlbnwwfHwwfHx8MA%3D%3D",
+      pendingMessage: 61,
+      location: "Delhi, India",
+      name: "Rohit",
+      age: 24,
+      intersts: [{
+         icon: `<i class="ri-music-2-line"></i>`,
+         interest: "music"
+      }, {
+         icon: `<i class="ri-quill-pen-fill"></i>`,
+         interest: "painting"
+      }],
+      bio: "Katie is a seasoned marketing professional with over 10 years of experience in digital advertising strategies. She has helped numerous brands achieve their online marketing goals, leading to increased visibility and 3x revenue Yo ",
+      isFriends: null
+
+   },
+
+   {
+      profilePic: "https://images.unsplash.com/photo-1513956589380-bad6acb9b9d4?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTh8fHBvdHJhaXR8ZW58MHx8MHx8fDA%3D",
+      displayPic: "https://images.unsplash.com/photo-1702353180890-bf0e6169fe75?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDJ8dG93SlpGc2twR2d8fGVufDB8fHx8fA%3D%3D",
+      pendingMessage: 61,
+      location: "Delhi, India",
+      name: "Ashwini",
+      age: 24,
+      intersts: [{
+         icon: `<i class="ri-music-2-line"></i>`,
+         interest: "music"
+      }, {
+         icon: `<i class="ri-quill-pen-fill"></i>`,
+         interest: "painting"
+      }],
+      bio: "Katie is a seasoned marketing professional with over 10 years of experience in digital advertising strategies. She has helped numerous brands achieve their online marketing goals, leading to increased visibility and 3x revenue Yo ",
+      isFriends: null
+
+   },
+
+   {
+      profilePic: "https://unsplash.com/photos/woman-wearing-black-crew-neck-shirt-3TLl_97HNJo",
+      displayPic: "https://images.unsplash.com/photo-1618151313441-bc79b11e5090?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTZ8fHBvdHJhaXR8ZW58MHx8MHx8fDA%3D",
+      pendingMessage: 61,
+      location: "Delhi, India",
+      name: "Vivek",
+      age: 24,
+      intersts: [{
+         icon: `<i class="ri-music-2-line"></i>`,
+         interest: "music"
+      }, {
+         icon: `<i class="ri-quill-pen-fill"></i>`,
+         interest: "painting"
+      }],
+      bio: "Katie is a seasoned marketing professional with over 10 years of experience in digital advertising strategies. She has helped numerous brands achieve their online marketing goals, leading to increased visibility and 3x revenue Yo ",
+      isFriends: null
+
+   },
+];
+
+function select(elem) {
+   return document.querySelector(elem);
+}
+
+let curr = 0;
+let isAnimating = false;
+
+function setData(index){
+   select(".prflimg img").src = users[index].profilePic;
+   select(".badge h5").textContent = users[index].pendingMessage;
+   select(".location h3").textContent = users[index].location;
+   select(".name h1:nth-child(1)").textContent = users[index].name;
+   select(".name h1:nth-child(2)").textContent = users[index].age;
+
+   var clutter = "";
+   users[index].intersts.forEach(function (interest) {
+      clutter += `<div class="tag flex items-center bg-white/30 px-3 py-1 rounded-full   gap-3"> ${interest.icon}
+    
+    <h3 class="text-sm text-lfg  tracking-tighter capitalized">${interest.interest}</h3>
+    </div>`;
+
+   });
+   select(".tags").innerHTML = clutter;
+
+   select(".bio p").textContent = users[index].bio;
+
+}
+
+(function setInitial() {
+   select(".maincard img").src = users[curr].displayPic;
+   select(".incomingcard img").src = users[curr + 1].displayPic;
+  
+   setData(curr);
+   curr = 2;
+})();
+
+
+
+function imageChange() {
+   if (!isAnimating) {
+      isAnimating = true;
+
+
+
+      let tl = gsap.timeline({
+         onComplete: function () {
+            isAnimating = false;
+            let main = select(".maincard");
+            let incoming = select(".incomingcard");
+
+            incoming.classList.remove("z-[2]");
+            incoming.classList.add("z-[3]");
+            incoming.classList.remove("incomingcard");
+
+            main.classList.remove("z-[3]");
+            main.classList.add("z-[2]");
+            gsap.set(main, {
+               scale: 1,
+               opacity: 1
+
+            })
+            if (curr === users.length) curr = 0;
+            select(".maincard img").src = users[curr].displayPic;
+            
+            curr++;
+            main.classList.remove("maincard");
+            incoming.classList.add("maincard");
+            main.classList.add("incomingcard");
+         }
+      });
+      tl.to(".maincard", {
+         scale: 1.1,
+         opacity: 0,
+         ease: Circ,
+         duration: .9
+      }, "a")
+         .from(".incoming", {
+            scale: .9,
+            opacity: 0,
+            ease: Circ,
+            duration: 1.1
+         }, "a")
+   }
+
+}
+let deny = select(".deny");
+let accept = select(".accept");
+
+deny.addEventListener("click", function () {
+   imageChange();
+
+   setData(curr-1);
+   gsap.from(".details .element",{
+      y: "100%",
+      opacity: 0,
+      stagger: .06,
+      ease: Power4.easeInOut,
+      duration: 1.5
+   })
+   
+});
+accept.addEventListener("click", function () {
+   imageChange();
+
+   setData(curr-1);
+   gsap.from(".details .element",{
+      y: "100%",
+      opacity: 0,
+      stagger: .06,
+      ease: Power4.easeInOut,
+      duration: 1.5
+   })
+   
+});
+
+
+(function containerCreator() {
+   document.querySelectorAll(".element")
+      .forEach(function (element) {
+         let div = document.createElement("div");
+         div.classList.add(`${element.classList[1]}container`, 'overflow-hidden');
+         console.log(div);
+         div.appendChild(element);
+         select(".details").appendChild(div);
+      })
+})();
+
